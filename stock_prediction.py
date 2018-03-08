9d8ea6fc7b1e7e3261f4ff7ab399193ed2e2feec
surelyourejoking <martin.robertandrew@gmail.com>
2018-03-08 16:20:19 +0800

    finished stock prediction

--- stock_prediction.py
+++ stock_prediction.py
@@ -1,33 +1,21 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from collections import Counter
-
-
-def status_calc(stock, sp500, outperformance=10):
-    """A simple function to classify whether a stock outperformed the S&P500
-    :param stock: stock price
-    :param sp500: S&P500 price
-    :param outperformance: stock is classified 1 if stock price > S&P500 price + outperformance
-    :return: true/false
-    """
-    if outperformance < 0:
-        raise ValueError("outperformance must be positive")
-    return stock - sp500 >= outperformance
+from utils import data_string_to_float, status_calc
 
 
 def build_data_set():
     """
     Reads the keystats.csv file and prepares it for scikit-learn
-
     :return: X_train and y_train numpy arrays
     """
-
     training_data = pd.read_csv("keystats.csv", index_col='Date')
+    training_data.dropna(axis=0, how='any', inplace=True)
     features = training_data.columns[6:]
 
     X_train = training_data[features].values
+    # Generate the labels: '1' if a stock beats the S&P500 by more than 10%, else '0'.
     y_train = list(map(
-        status_calc, training_data["stock_p_change"], training_data["sp500_p_change"]))
+        status_calc, training_data["stock_p_change"], training_data["SP500_p_change"]))
 
     return X_train, y_train
 
@@ -40,15 +28,21 @@ def predict_stocks():
 
     # Now we get the actual data from which we want to generate predictions.
     data = pd.read_csv('forward_sample.csv', index_col='Date')
+    data.dropna(axis=0, how='any', inplace=True)
     features = data.columns[6:]
     X_test = data[features].values
     z = data["Ticker"].values
 
     # Get the predicted tickers
     y_pred = clf.predict(X_test)
-    invest_list = z[y_pred].tolist()
-
-    return invest_list
+    if sum(y_pred) == 0:
+        print("No stocks predicted!")
+    else:
+        invest_list = z[y_pred].tolist()
+        print(
+            f"{len(invest_list)} stocks predicted to outperform the S&P500 by more than 10%:")
+        print(' '.join(invest_list))
+        return invest_list
 
 
 if __name__ == '__main__':
